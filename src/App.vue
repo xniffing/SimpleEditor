8d53c5f1b8103db7e08701e9902ae3729c6c3246
David Joao Da Silva Vicente <david.vicente@siemens.com>
2020-03-24 15:25:06 +0000

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section>
+        <codemirror class="editor"
+                ref="myCm"
+                :value="code"
+                :options="cmOptions"
+                @ready="onCmReady"
+                @focus="onCmFocus"
+                @input="onCmCodeChange"
+        >
+        </codemirror>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // language js
+    import "codemirror/mode/javascript/javascript.js";
+    // theme css
+    import "codemirror/theme/base16-dark.css";
+    // more codemirror resources
+    // import 'codemirror/some-resource...'
+    export default {
+        data() {
+            return {
+                code: ` ---
+- hosts: webservers
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  vars:
+    http_port: 80
+    max_clients: 200
+
+  remote_user: root
+
+  tasks:
+  - name: ensure apache is at the latest version
+    yum:
+      name: httpd
+      state: latest
+
+  - name: write the apache config file
+    template:
+      src: /srv/httpd.j2
+      dest: /etc/httpd.conf
+    notify:
+    - restart apache
+
+  - name: ensure apache is running
+    service:
+      name: httpd
+      state: started
+
+  handlers:
+    - name: restart apache
+      service:
+        name: httpd
+        state: restarted `,
+                cmOptions: {
+                    // codemirror options
+                    tabSize: 4,
+                    mode: "text/javascript",
+                    theme: "base16-dark",
+                    lineNumbers: true,
+                    line: true
+                    // more codemirror options, 更多 codemirror 的高级配置...
+                }
+            };
+        },
+        methods: {
+            onCmReady(cm) {
+                console.log("the editor is readied!", cm);
+            },
+            onCmFocus(cm) {
+                console.log("the editor is focus!", cm);
+            },
+            onCmCodeChange(newCode) {
+                console.log("this is new code", newCode);
+                this.code = newCode;
+            }
+        },
+        computed: {
+            codemirror() {
+                return this.$refs.myCm.codemirror;
+            }
+        },
+        mounted() {
+            console.log("this is current codemirror object", this.codemirror);
+            // you can use this.codemirror to do something...
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    body, html {
+        margin: 0;
+        height: 100%;
+    }
+    .CodeMirror.cm-s-base16-dark{
+        height: 100vh;
+    }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueCodemirror from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueCodemirror, {
+    theme: "base16-dark"
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount("#app");
